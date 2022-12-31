ecf87dc6574041606fd1763e9749f834d03c9733
MATHEUS MENDONCA MAIGNARDI <70504309+Maignardi@users.noreply.github.com>
2022-12-30 23:18:15 -0400

    first commit

---
+++ app/src/main/java/com/example/agenda_room/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.agenda_room
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.agenda_room.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private  var mAdapter: UserAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        abrirAdd()
+    }
+
+    private fun abrirAdd() {
+        binding.btnAdd.setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun setAdapter(list:List<User>) {
+        mAdapter?.setData(list)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleScope.launch {
+            val userList = AppDatabase(this@MainActivity).getUserDao().getAllUser()
+
+            mAdapter = UserAdapter()
+            binding.recyclerView.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = mAdapter
+                setAdapter(userList)
+                mAdapter?.setOnActionEditListener {
+                    val intent = Intent(this@MainActivity, AddActivity::class.java)
+                    intent.putExtra("Data", it)
+                    startActivity(intent)
+                }
+
+                mAdapter?.setOnActionDeleteListener {
+                    val builder = AlertDialog.Builder(this@MainActivity)
+                    builder.setMessage("Are you sure you want to delete")
+                    builder.setPositiveButton("Yes") { p0, p1 ->
+                        lifecycleScope.launch{
+                            AppDatabase(this@MainActivity).getUserDao().deleteUser(it)
+                            val list = AppDatabase(this@MainActivity).getUserDao().getAllUser()
+                            setAdapter(list)
+                        }
+                        p0.dismiss()
+                    }
+                    builder.setNegativeButton("No") { p0,p1 ->
+                        p0.dismiss()
+                    }
+                    val dialog = builder.create()
+                    dialog.show()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/agenda_room/User.kt
@@ -0,0 +1,14 @@
+package com.example.agenda_room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+
+@Entity
+data class User(
+    var firstName: String = "",
+    var lastName: String = ""
+): Serializable {
+    @PrimaryKey(autoGenerate = true) var id:Int = 0
+}
